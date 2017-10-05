39d5ead76ec9f7d3613fbcb9ab07141783da4030
Sivli Kestanous <cmtoups@me.com>
2017-10-05 14:14:00 -0400

    build

--- src/app.js
+++ src/app.js
@@ -24,12 +24,19 @@ class App extends React.Component {
         <div>
           <ul>
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/example1">example1</Link></li>
+            <li><Link to="/example2">example2</Link></li>
           </ul>
           <Route exact path="/" component={HomePage}/>
+          <Route exact path="/example1" component={example1}/>
+          <Route exact path="/example2" component={example2}/>
         </div>
   		</Router>
     );
   }
 }
+
+const example1 = () => <div>example1</div>
+const example2 = () => <div>example2</div>
 
 render(<App/>, document.getElementById('app'));
