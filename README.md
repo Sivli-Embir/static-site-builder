7ccf26006c38c17dc6ede0cce726dd1d05336611
Sivli Kestanous <cmtoups@me.com>
2017-10-05 13:56:41 -0400

    testing router

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # static-site-builder
+
+start with
+```yarn```
+
+develop with
+```yarn dev```
+
+run as server
+```cd docs && python -m SimpleHTTPServer 3000```
+
+deploy
+```yarn build && git add . && git commit -m 'build' && git push```
--- src/app.js
+++ src/app.js
@@ -4,14 +4,25 @@ import {render} from 'react-dom';
 import styles from './app.css';
 import fonts from './font.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import HomePage from "./components/home-page.js";
 
 class App extends React.Component {
   render () {
     return (
-      <div id="top">
-        <HomePage />
-  		</div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+          </ul>
+          <Route exact path="/" component={HomePage}/>
+        </div>
+  		</Router>
     );
   }
 }
